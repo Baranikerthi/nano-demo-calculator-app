42b5950c6f5e8ee43a837bdaae2dd0c405716a1d
Baranikerthi <79745315+Baranikerthi@users.noreply.github.com>
2023-09-05 19:32:20 +0000

    completed

--- server.py
+++ server.py
@@ -11,28 +11,21 @@ app = Flask(__name__)
 
 def greeting():
     '''function to add'''
-    return jsonify("Hello world"),200
+    return "Hello world"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-
-    data =request.get_json()
-    if "first" in data and "second" in data:
-        f = data["first"]
-        s = data["second"]
-        result={"result":f+s}
-        
-
-        return jsonify(result),200
+    num = request.json
+    first = num['first']
+    second = num['second']
+    return jsonify({'result': first+second}), 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    data =request.get_json()
-    if "first" in data and "second" in data:
-        f = data["first"]
-        s = data["second"]
-        result={"result":f-s}
-        return jsonify(result),200
+    num = request.json
+    first = num['first']
+    second = num['second'] 
+    return jsonify({'result': first-second}), 200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
