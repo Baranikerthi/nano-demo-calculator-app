5095dc1c00ae7b242b4d4e7d79348b1468a35b11
Baranikerthi <79745315+Baranikerthi@users.noreply.github.com>
2023-09-05 19:31:54 +0000

    completed

--- server.py
+++ server.py
@@ -1,24 +1,33 @@
-from flask import Flask
+'''
+import required 
+'''
+from flask import Flask,request,jsonify
+
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
+
 def greeting():
+    '''function to add'''
     return jsonify("Hello world"),200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
+
     data =request.get_json()
     if "first" in data and "second" in data:
         f = data["first"]
         s = data["second"]
         result={"result":f+s}
+        
+
         return jsonify(result),200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-   data =request.get_json()
+    data =request.get_json()
     if "first" in data and "second" in data:
         f = data["first"]
         s = data["second"]
