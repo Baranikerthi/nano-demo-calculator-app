5ffaab393a6e2e9c00741d2f75bcf34190c4c761
Baranikerthi <79745315+Baranikerthi@users.noreply.github.com>
2023-09-05 18:37:33 +0000

    run

--- server.py
+++ server.py
@@ -5,15 +5,25 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return jsonify("Hello world"),200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data =request.get_json()
+    if "first" in data and "second" in data:
+        f = data["first"]
+        s = data["second"]
+        result={"result":f+s}
+        return jsonify(result),200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+   data =request.get_json()
+    if "first" in data and "second" in data:
+        f = data["first"]
+        s = data["second"]
+        result={"result":f-s}
+        return jsonify(result),200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
